0b0994856f359d8cd659f7c9cc2dd0823af26f2c
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2022-01-24 17:22:03 +0100

    cors

--- config/index.js
+++ config/index.js
@@ -22,7 +22,6 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      credentials: true,
       origin: process.env.ORIGIN || "https://edu-vadillo-portafolio.herokuapp.com",
     })
   );
